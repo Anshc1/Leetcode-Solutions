0b6794e2f8ef06bbfc939ea02017722ac801869c
Ansh chaturvedi <78293897+Anshc1@users.noreply.github.com>
2023-09-02 01:31:49 +0530

    Time: 6 ms (56.45%), Space: 7.8 MB (89.85%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>a(n+1 , 0 ); 
+        for(int i= 1; i <= n ; i++){
+            a[i] = a[i/2] ; 
+            a[i]+= i%2; 
+        }
+        return a  ;
+    }
+};
