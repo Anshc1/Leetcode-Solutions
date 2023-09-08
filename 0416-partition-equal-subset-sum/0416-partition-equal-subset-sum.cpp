38f7d63b1f081b7ebe28b379c70c186b55aaa947
Ansh chaturvedi <78293897+Anshc1@users.noreply.github.com>
2023-09-08 10:33:54 +0530

    Time: 222 ms (67.46%), Space: 27.1 MB (55.60%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -6,12 +6,14 @@ public:
         for (int i = 0 ; i < n ; i++) {
             sum += a[i];
         }
-        vector<vector<int>>dp(n + 1, vector<int>(sum + 1, 0));
+        int dp[n+1][sum+1]; 
+        memset(dp , 0 , sizeof(dp)); 
         dp[0][0] = 1;
+        
         for (int i = 0; i < n ; i++) {
             for (int k = 0 ; k <= sum ; k++) {
                 dp[i + 1][k] |= dp[i][k];
-                if (dp[i][k]) {
+                if (dp[i][k] && k+a[i] <= sum) {
                     dp[i + 1][k + a[i]] |= 1 ;
                 }
             }
