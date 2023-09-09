ddd0fdcbf76179b52de15db17ea9ed19d042bd86
Ansh chaturvedi <78293897+Anshc1@users.noreply.github.com>
2023-09-09 09:42:35 +0530

    Time: 2469 ms (5.03%), Space: 334.5 MB (13.22%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,58 +1,23 @@
 class Solution {
 public:
-    class Tries {
-        public : 
-        Tries* children[2] ={NULL, NULL};
-    
-        
-        void insert(int num ) {
-            Tries* root = this ;
-            for (int j = 30 ; j >= 0 ; j--) {
-                int bit =(num >> j )&1; 
-                if(root->children[bit] == NULL){
-                    Tries*curr =new Tries(); 
-                    root->children[bit] = curr;
-                    root = curr; 
-                }else{
-                    root = root->children[bit]; 
+    int findMaximumXOR(vector<int>& nums) {
+        int n = nums.size(); 
+        int mask =0 ; 
+        int ans =0 ; 
+        for(int i =30 ;i >= 0  ; i--){
+            mask|= 1<<i ; 
+            set<int>x ;
+            for(auto j : nums){
+                x.insert(j&mask); 
+            }
+            int curr  = ans|(1<<i);
+            for(auto j : x){
+                if(x.find(j^curr)!= x.end()){
+                    ans= curr; 
+                    break; 
                 }
             }
         }
-        bool find(int num ){
-            Tries *root = this; 
-            for(int j = 30 ;j>= 0; j--){
-               int bit = (num>>j)&1 ; 
-               if(root->children[bit] == NULL){
-                   return 0 ; 
-               }else{
-                   root = root->children[bit]; 
-               }
-            }
-            return 1; 
-        }
-    };
-    int findMaximumXOR(vector<int>& a) {
-        
-        int n = a.size(); 
-        Tries *root = new Tries(); 
-        for(auto i : a){
-            root->insert(i); 
-        }
-        int ans = 0 ; 
-        for(int i =0 ;i < n ;i++){
-            int res = 0; 
-            Tries* curr = root; 
-            for(int j = 30 ; j>= 0 ; j--){
-                bool bit = (a[i]>>j)&1;
-                if(curr->children[!bit]==NULL){
-                    curr = curr->children[bit];
-                }else{
-                    curr = curr->children[!bit];
-                    res+= 1<<j ; 
-                }
-            }
-            ans =max(ans,res); 
-        }
-        return ans ;  
+        return ans; 
     }
 };
