dba17c227976c4432b1c20a0f8b7a81357347b32
Ansh chaturvedi <78293897+Anshc1@users.noreply.github.com>
2023-09-28 15:41:33 +0530

    Time: 310 ms (16.98%), Space: 118.6 MB (6.61%) - LeetHub

---
+++ 2862-maximum-element-sum-of-a-complete-subset-of-indices/2862-maximum-element-sum-of-a-complete-subset-of-indices.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    unordered_map<int,int> primes(int n ){
+        unordered_map<int,int>x; 
+        for(int i = 2 ;i*i <= n ; i++){
+            while(n%i == 0 ){
+                x[i]++;
+                n/=i ; 
+            }
+        }
+        if(n >= 2){
+            x[n]++; 
+        }
+        return x ;
+    }
+    
+    long long maximumSum(vector<int>& a) {
+        int n = a.size(); 
+        map<int,long long>ct;
+        long long ans = 0; 
+        for(int i =0 ; i < n ; i++){
+            auto mp = primes(i+1);
+            int val = 1; 
+            for(auto j : mp){
+                if(j.second%2){
+                    val*= j.first; 
+                }
+            }
+            ct[val]+= a[i]; 
+            ans = max(ans , ct[val]); 
+        }
+        return ans; 
+    }
+    
+};
