a2df6d5a5391a2054a0038a09e817a00619fa616
Ansh chaturvedi <78293897+Anshc1@users.noreply.github.com>
2023-10-03 19:19:38 +0530

    Time: 426 ms (5.07%), Space: 108.6 MB (5.69%) - LeetHub

--- 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.cpp
+++ 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int find_seg(int l , int  r , vector<int>&a){
-        //cout<< l << " " << r<<endl;  
-        multiset<int>x; 
-        for(int i =l  ; i <= r ; i ++){
-            x.insert(a[i]); 
+    int find_seg(int l , int  r , vector<int>&a){  
+        unordered_map<int,int>x; 
+        int mn = 1e9 ; 
+        for(int i =r  ; i >= l  ; i-- ){
+            mn = min(mn , a[i]); 
+            x[i] = mn; 
         }
         int mx = 0 ; 
         int ans =1 ; 
         for(int i = l  ;i <  r ; i++){
-            x.erase(x.find(a[i]));
             mx = max(mx , a[i]); 
-            if(mx <= *x.begin()){
+            if(mx <= x[i+1] ) {
                 ans = find_seg(l , i ,a ) + find_seg(i+1, r ,a); 
                 break ;
             }
