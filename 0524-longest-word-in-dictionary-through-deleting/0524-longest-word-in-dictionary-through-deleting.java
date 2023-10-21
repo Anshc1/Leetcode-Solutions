c3dd82751fc1abb31b53bb475feb0c4ea8f62819
Ansh chaturvedi <78293897+Anshc1@users.noreply.github.com>
2023-10-21 17:06:18 +0530

    Time: 16 ms (79.12%), Space: 44.2 MB (58.03%) - LeetHub

--- 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.java
+++ 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.java
@@ -1,33 +1,32 @@
+import java.util.List;
+
 public class Solution {
-    public String findLongestWord(String s, List<String> a) {
-        int n = a.size();
-        int m = s.length();
-        int[] idx = new int[n];
-        int[] ct = new int[n];
-        int mx = 0;
-        List<String> x = new ArrayList<>();
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i < s.length() && idx[j] < a.get(j).length() && s.charAt(i) == a.get(j).charAt(idx[j])) {
-                    idx[j]++;
-                }
-                if (idx[j] == a.get(j).length()) {
-                    mx = Math.max(mx, a.get(j).length());
-                    x.add(a.get(j));
+    
+    public String findLongestWord(String s, List<String> dictionary) {
+        String result = "";
+        
+        for (String word : dictionary) {
+            if (isSubsequence(s, word)) {
+                if (word.length() > result.length() || 
+                    (word.length() == result.length() && word.compareTo(result) < 0)) {
+                    result = word;
                 }
             }
         }
-        if (mx == 0) {
-            return "";
-        }
+        
+        return result;
+    }
 
-        String ans = String.format("%1$-" + 1005 + "s", "z").replace(' ', 'z');
-        for (String i : x) {
-            if (i.length() == mx) {
-                ans = (ans.compareTo(i) < 0) ? ans : i;
+    private boolean isSubsequence(String s, String word) {
+        int i = 0, j = 0;
+        
+        while (i < s.length() && j < word.length()) {
+            if (s.charAt(i) == word.charAt(j)) {
+                j++;
             }
+            i++;
         }
-        return ans;
+        
+        return j == word.length();
     }
 }
