469ff5df7399177dd9d264143210f4949185aa6b
Ansh chaturvedi <78293897+Anshc1@users.noreply.github.com>
2023-12-28 17:47:27 +0530

    Time: 12 ms (19.40%), Space: 9.8 MB (54.15%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -19,18 +19,25 @@ public:
         int l = 0 , r = a.size()-1; 
         while(l <= r){
             int mid = (l+r)>>1; 
-            bool check = bs(a[mid] , t);
-            //cout << mid << " " << check <<endl; 
-            if(check){
-                return 1 ; 
-            }else{
-                if(t < a[mid][0] ){
-                    r = mid-1; 
+            if(t >= a[mid][0] && t <= a[mid].back() ){
+                bool check = bs(a[mid] , t);
+                if(check){
+                    return 1; 
                 }else{
-                    l = mid+1; 
+                    return 0 ; 
                 }
+            }
+            else if(t < a[mid][0] ){
+                r = mid-1; 
+            }else{
+                l = mid+1; 
             }
         }
         return 0 ; 
     }
+    // 0 9 
+    //5 
+    //2
+    //1
+    
 };
