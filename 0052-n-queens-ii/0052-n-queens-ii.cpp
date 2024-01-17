b91e67685b445b99ecc59da25accca3f5652bbdf
Ansh chaturvedi <78293897+Anshc1@users.noreply.github.com>
2024-01-18 00:59:45 +0530

    Time: 2 ms (83.67%), Space: 6.6 MB (48.03%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -6,35 +6,32 @@ public:
         b-= y; 
         return ((abs(a) == abs(b)) ||(a == 0 ) || (b== 0 )) ;
     }
-    int recur(int i , vector<vector<int>>&a  , int t  ){
+    int recur(int i , int j , vector<pair<int,int>>&a  , int t  ){
         if(t == 0 ){
             return 1; 
         }
-        if(i >=n ){
+        if(i >=n || j>= n ){
             return 0 ; 
         }
-        int res  = 0 ;  
-        for(int jj = 0; jj < n ; jj++){
-            bool ok = 1 ; 
-            for(int xx = 0 ; xx < n ; xx++){
-                for(int yy = 0 ; yy < n ; yy++){
-                    if(a[xx][yy]  && check(i , jj , xx , yy)){
-                        ok = 0 ; 
-                    }
-                }
-            }
-            if(ok){
-                a[i][jj] =1 ;  
-                res+= recur(i+1, a , t-1); 
-                a[i][jj] =0 ;  
+        int res  = 0 ; 
+        res+= recur(i , j+1 , a , t);
+        bool ok = 1; 
+        for(auto p : a){
+            if(check(p.first , p.second , i , j)){
+                ok = 0; 
+                break; 
             }
         }
-        
+        if(ok){
+            a.push_back({i, j}); 
+            res+= recur(i+1 , 0 , a , t-1); 
+            a.pop_back(); 
+        }
         return res; 
     }
     int totalNQueens(int n) {
         this->n = n ; 
-        vector<vector<int>>a(n , vector<int>(n , 0)) ; 
-        return recur(0  , a , n); 
+        vector<pair<int,int>>a ; 
+        return recur(0 , 0 , a , n); 
     }
 };
